c4d20d60ac5e50ee2330e061d277c3e9222fa738
Steve Nosse <nossesteve@gmail.com>
2021-09-11 14:17:53 +0200

    Now possible to customise the delay between taps

--- lib/double_tap_to_exit.dart
+++ lib/double_tap_to_exit.dart
@@ -9,11 +9,22 @@ class DoubleTapToExit extends StatefulWidget {
   const DoubleTapToExit({
     Key? key,
     required this.child,
+    this.tapDelay,
     this.message,
   }) : super(key: key);
 
+  /// The message to display when notifying the user
+  /// they should tap twice to close the app
+  /// Default: "Hit back button again to quit"
   final String? message;
+
+  /// Child
+  /// Should normally be the content of the page
   final Widget child;
+
+  /// Delay between the first tap and the second one
+  /// Default: Duration(seconds: 1) => 1 second.
+  final Duration? tapDelay;
 
   @override
   _DoubleTapToExitState createState() => _DoubleTapToExitState();
@@ -39,7 +50,7 @@ class _DoubleTapToExitState extends State<DoubleTapToExit> {
             text: widget.message ?? "Hit back button again to quit",
             duration: ToastDuration.long,
           );
-          Future.delayed(Duration(seconds: 1)).then((value) {
+          Future.delayed(widget.tapDelay ?? Duration(seconds: 1)).then((value) {
             _tappedTimes = 0;
           });
         } else if (_tappedTimes == 2) {
